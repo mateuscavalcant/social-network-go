32381a53e176bded19d042501fcc3f58b28a3f49
Mateus <mateuscavalcant7@gmail.com>
2023-08-04 15:42:02 -0300

    Add Model

---
+++ pkg/model/model.go
@@ -0,0 +1,26 @@
+package models
+
+type User struct {
+	ID              int    `json:"id"`
+	Username        string `json:"username" binding:"required, min=4,max=32"`
+	Email           string `json:"email" binding:"required, email"`
+	Password        string `json:"password" binding:"required, min=8, max=16"`
+	ConfirmPassword string `json:"cpassword" binding:"required"`
+}
+
+type Login struct {
+	Credential string `json:"credential"`
+	Password   string
+}
+
+type Follow struct {
+	FollowID int `json:"follow-id"`
+	FollowBy int `json:"follow-by"`
+	FolloTo  int `json:"follow-to"`
+}
+
+type Post struct {
+	PostID  int    `json:"post-id"`
+	UserID  int    `json:"user-id"`
+	Content string `json:"content"`
+}
