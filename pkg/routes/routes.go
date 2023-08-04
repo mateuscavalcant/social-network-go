c214ef2cbf605844ab21b9056cd968aca23fc73e
Mateus <mateuscavalcant7@gmail.com>
2023-08-04 16:04:56 -0300

    Add Routes

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,9 +1,8 @@
 package routes
 
 import (
-	"social-network-go/pkg/controller"
 	"social-network-go/pkg/controller/auth"
-	"social-network-go/pkg/controller/post"
+	"social-network-go/pkg/controller/home"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,7 +11,7 @@ func InitRoutes(r *gin.RouterGroup) {
 	r.POST("/signup", auth.Signup)
 	r.POST("/validate-email", auth.ExistEmail)
 	r.POST("/login", auth.UserLogin)
-	r.POST("/create-post", post.CreateNewPost)
-	r.GET("/feed", controller.Home)
+	r.POST("/create-post", home.CreateNewPost)
+	r.GET("/feed", home.Feed)
 	r.GET("/loggout", auth.Logout)
 }
